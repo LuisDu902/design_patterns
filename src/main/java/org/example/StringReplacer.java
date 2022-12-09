5b19f7afddaa481a064fbdea2c9bd3d81c8c9554
Du <luispedro.2003@hotmail.com>
2022-12-09 16:33:16 +0000

    Command pattern

---
+++ src/main/java/org/example/StringReplacer.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class StringReplacer implements StringTransformer{
+    private char a;
+    private char b;
+    public StringReplacer(char a, char b){
+        this.a = a;
+        this.b = b;
+    }
+    public void execute(StringDrink drink){
+        drink.setText(drink.getText().replace(a,b));
+    }
+    public void undo(StringDrink drink){
+        drink.setText(drink.getText().replace(b,a));
+    }
+}
